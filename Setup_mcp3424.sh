2be4814eff117edb6051ced7799cd6746b6bf8f9
Peter Sjoberg <peters-pi8@techwiz.ca>
2019-07-05 22:47:16 -0400

    fixed init order and path names

--- Setup_mcp3424.sh
+++ Setup_mcp3424.sh
@@ -5,11 +5,12 @@
 #	Created
 #
 
-echo "Load mcp3424 module:"
-lsmod|grep -q mcp3422 || sudo modprobe mcp3422
 
 addresses=$(i2cdetect -y  1|awk '/^60:/{print $10,$11,$12,$13,$14,$15,$16,$17}'|xargs -n1|grep -vE "UU|--")
 [ -z "$addresses" ] && addresses=6e
+
+echo "Load mcp3424 module:"
+lsmod|grep -q mcp3422 || sudo modprobe mcp3422
 
 for address in $addresses;do
     [ -e /sys/bus/i2c/devices/i2c-1/device/i2c-1/1-00$address ] || echo mcp3424 0x$address|sudo tee -a /sys/bus/i2c/devices/i2c-1/new_device
@@ -20,14 +21,14 @@ for address in $addresses;do
         exit
     fi
 
-    sudo chown pi /sys/bus/i2c/devices/1-00${address}/iio\:device0/*
+    sudo chown pi /sys/bus/i2c/devices/1-00${address}/iio\:device*/*
     #echo "setup scale on $address:"
     #echo 3 >/sys/bus/i2c/devices/1-00${address}/iio:device0/in_voltage_sampling_frequency  # 18 bit 
     #echo 0.000001953 >/sys/bus/i2c/devices/1-00${address}/iio\:device0/in_voltage0_scale   # more gain
 
     echo "values on $address"
     for port in {0..3};do
-        v=$(echo $(cat /sys/bus/i2c/devices/1-00${address}/iio:device0/in_voltage${port}_raw)*$(cat /sys/bus/i2c/devices/1-00${address}/iio:device0/in_voltage${port}_scale)|bc -l)
+        v=$(echo $(cat /sys/bus/i2c/devices/1-00${address}/iio:device*/in_voltage${port}_raw)*$(cat /sys/bus/i2c/devices/1-00${address}/iio:device*/in_voltage${port}_scale)|bc -l)
         echo "$port: $v  $(echo $v*2|bc -l) $(echo $v*3|bc -l)"
     done
 done
